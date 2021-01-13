8828d0d926c1c1a07619473752943e9d89448b18
Roman Belik <rombelik@yandex.ru>
2021-01-13 16:54:15 +0300

    added reading files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -7,9 +7,9 @@ const {
   CliCommand,
   DEFAULT_GENERATE_COUNT,
   MOCK_FILE_NAME,
-  MOCK_TITLES,
-  MOCK_SENTENCES,
-  MOCK_CATEGORIES,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH,
+  FILE_SENTENCES_PATH,
   MAX_MOCK_ITEMS,
   OfferType,
   SumRestrict,
@@ -27,21 +27,33 @@ const getPictureFileName = (number) => {
   return number < 10 ? `item0${number}.jpg` : `item${number}.jpg`;
 };
 
-const generateOffers = (count) => {
+const generateOffers = async (count, titles, categories, sentences) => {
   return Array(count).fill({}).map(() => ({
-    title: MOCK_TITLES[getRandomInt(0, MOCK_TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    description: shuffle(MOCK_SENTENCES).slice(SentencesRestrict.MIN, SentencesRestrict.MAX).join(` `),
+    description: shuffle(sentences).slice(SentencesRestrict.MIN, SentencesRestrict.MAX).join(` `),
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
-    category: shuffle(MOCK_CATEGORIES).slice(getRandomInt(0, MOCK_CATEGORIES.length - 1)),
+    category: shuffle(categories).slice(getRandomInt(0, categories.length - 1)),
   }));
+};
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    const result = content.trim().split(`\n`);
+    console.info(chalk.green(`Данные в количестве ${result.length} фраз успешно считаны`));
+    return result;
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
 };
 
 const writeOffers = async (offers) => {
   try {
     await fs.writeFile(MOCK_FILE_NAME, JSON.stringify(offers));
-    console.info(chalk.green(`Данные в количестве [${offers.length}] успешно сформированы в файл ${MOCK_FILE_NAME}`));
+    console.info(chalk.green(`Данные в количестве ${offers.length} успешно сформированы в файл ${MOCK_FILE_NAME}`));
     process.exit(ExitCode.SUCCESS);
   } catch (err) {
     console.info(chalk.red(`Ошибка при создании данных`, err));
@@ -51,7 +63,7 @@ const writeOffers = async (offers) => {
 
 module.exports = {
   name: CliCommand.GENERATE,
-  run(args = []) {
+  async run(args = []) {
     const [userCount] = args;
     const count = Number.parseInt(userCount, 10);
     const countOffer = count && count > 0 ? count : DEFAULT_GENERATE_COUNT;
@@ -60,8 +72,10 @@ module.exports = {
       console.info(chalk.red(`Не больше ${MAX_MOCK_ITEMS} объявлений`));
       process.exit(ExitCode.FATAL_EXCEPTION);
     }
-
-    const offers = generateOffers(countOffer);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const offers = await generateOffers(countOffer, titles, categories, sentences);
     writeOffers(offers);
   }
 };
